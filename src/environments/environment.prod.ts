e50f621ba60a6780beb7d7f12429deace14d7a41
RomexpertSRL <84737442+RomexpertSRL@users.noreply.github.com>
2022-03-21 19:39:19 +0200

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBtWiYUmQtqiLkKrEj3Gjzm0m1IMXH5n4w",
+    authDomain: "medcloud-5d858.firebaseapp.com",
+    projectId: "medcloud-5d858",
+    storageBucket: "medcloud-5d858.appspot.com",
+    messagingSenderId: "789941115401",
+    appId: "1:789941115401:web:23dc8fd062e1c79c6a36d8",
+    measurementId: "G-GDZWNEM4B3",
+  agora: {
+    appId: 'fa5408ea1a0c48ba93f2885b57417453'
+  }
+  }
+
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,31 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBtWiYUmQtqiLkKrEj3Gjzm0m1IMXH5n4w",
+    authDomain: "medcloud-5d858.firebaseapp.com",
+    projectId: "medcloud-5d858",
+    storageBucket: "medcloud-5d858.appspot.com",
+    messagingSenderId: "789941115401",
+    appId: "1:789941115401:web:23dc8fd062e1c79c6a36d8",
+    measurementId: "G-GDZWNEM4B3",
+  agora: {
+    appId: 'fa5408ea1a0c48ba93f2885b57417453'
+  }
+  }
+  
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
